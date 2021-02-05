69caaa7ad73a3047ec25c480afdbde8ffa87d37c
adarsht756 <adarsht756>
2021-02-05 11:08:53 +0530

    added 2nd AJP practical

--- Practical-02/client.java
+++ Practical-02/client.java
@@ -1,23 +1,17 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
-public class client {
-    public static void main(String args[]) throws Exception {
-        Socket s = new Socket("localhost", 3333);
-        DataInputStream din = new DataInputStream(s.getInputStream());
-        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String str = "", str2 = "";
-        while (!str.equals("stop")) {
-            str = br.readLine();
-            dout.writeUTF(str);
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("192.168.44.188", 8080);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF("Hello server");
             dout.flush();
-            str2 = din.readUTF();
-            System.out.println("Server says: " + str2);
+            dout.close();
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
-
-        dout.close();
-        s.close();
     }
 }
--- Practical-02/server.java
+++ Practical-02/server.java
@@ -1,24 +1,17 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
-public class server {
-    public static void main(String args[]) throws Exception {
-        ServerSocket ss = new ServerSocket(3333);
-        Socket s = ss.accept();
-        DataInputStream din = new DataInputStream(s.getInputStream());
-        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String str = "", str2 = "";
-        while (!str.equals("stop")) {
-            str = din.readUTF();
-            System.out.println("client says: " + str);
-            str2 = br.readLine();
-            dout.writeUTF(str2);
-            dout.flush();
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(8080);
+            Socket s = ss.accept(); // establishes connection
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            String str = (String) dis.readUTF();
+            System.out.println("Message from the Client is: " + str);
+            ss.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        din.close();
-        s.close();
-        ss.close();
     }
 }
